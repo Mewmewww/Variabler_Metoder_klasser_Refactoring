93f94368711e427c376191688791113ae20b8d7a
Deniz Yandim <Deniz.yandim00@gmail.com>
2020-09-16 16:13:29 +0200

    Initial commit

---
+++ src/Moms.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Moms {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Indtast et vilkårligt køb-beløb");
+
+        double koebmaengde = input.nextDouble();
+
+        /*Danish tax(moms) is 25% - therefore 0.25
+        Additionally, the reason why we times and divide by 100 is simply to first round down our variable, Moms
+        to two digits. */
+
+        double moms = koebmaengde * 0.25;
+        double tyskmoms = koebmaengde * 0.16;
+        System.out.println("Dansk salgs moms er: " + (int)(plusDanskMoms(moms)) + " kroner");
+        System.out.println("Tysk salgs-moms er: " + (int)(plusTyskmoms(tyskmoms)) + " euro");
+    }
+    /*
+       Additionally, we could separate our moms method and make it independant.
+       Also for the last task, it was stated to rename our plusMoms to plusDanskMoms - so just be aware that
+       I've done the test for both plusMoms and plusDanskmoms.
+    */
+    public static double plusDanskMoms(double moms) {
+        return moms;
+    }
+    // Again, we can make one for german moms.
+    public static double plusTyskmoms(double tyskmoms) {
+        return tyskmoms;
+    }
+}
---
+++ src/Variabler.java
@@ -0,0 +1,21 @@
+public class Variabler {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 10245;
+        int i = 2147483647;
+        long l = 932932939;
+        float f = 0.712382f;
+        double d = 12248282;
+        boolean bool = true;
+        char c = 16;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+        System.out.println(c);
+    }
+}
